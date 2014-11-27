df99bfa7dcfceb71ee7f83848f85f5a49695f1a2
mkuwamura <mk2row@yahoo.co.jp>
2014-11-27 16:51:19 +0900

    first up

---
+++ ssl_expire_check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#author @mkuwamura
+#
+
+echo "çXêVì˙" `date`"<BR>"
+
+while read DOMAIN
+do
+date +"%s" --date="`openssl s_client -connect "$DOMAIN":443 -servername "$DOMAIN" </dev/null 2>/dev/null | openssl x509 -enddate -noout | cut -d'=' -f2`" | gawk '{printf("%d\n",($0-systime())/86400-1/86400+1)}'| tr '\n' ', '
+echo -e "\t$DOMAIN<BR>"
+echo -e "\r\n"
+done <check_url_list
+

28bd5dd9af6dfc4973eb96ba0dc7602f9584bfa9
mkuwamura <mk2row@yahoo.co.jp>
2014-11-27 17:13:27 +0900

    url list add

--- ssl_expire_check.sh
+++ ssl_expire_check.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+Ôªø#!/bin/bash
 #author @mkuwamura
 #
 
-echo "çXêVì˙" `date`"<BR>"
+echo "check_date" `date`"<BR>"
 
 while read DOMAIN
 do
-date +"%s" --date="`openssl s_client -connect "$DOMAIN":443 -servername "$DOMAIN" </dev/null 2>/dev/null | openssl x509 -enddate -noout | cut -d'=' -f2`" | gawk '{printf("%d\n",($0-systime())/86400-1/86400+1)}'| tr '\n' ', '
-echo -e "\t$DOMAIN<BR>"
-echo -e "\r\n"
+  date +"%s" --date="`openssl s_client -connect "$DOMAIN":443 -servername "$DOMAIN" </dev/null 2>/dev/null | openssl x509 -enddate -noout | cut -d'=' -f2`" | gawk '{printf("%d\n",($0-systime())/86400-1/86400+1)}'| tr '\n' ', '
+  echo -e "\t$DOMAIN<BR>"
+  echo -e "\r\n"
 done <check_url_list
 

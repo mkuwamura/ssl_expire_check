459590632f8358c3c168deaa8f7d5ab4526cfb7b
mkuwamura <mk2row@yahoo.co.jp>
2014-11-30 20:48:08 +0900

    bug atack

--- ssl_expire_check.sh
+++ ssl_expire_check.sh
@@ -1,7 +1,7 @@
-﻿#!/bin/bash
+#!/bin/bash
 #author @mkuwamura
 #
-
+SCRIPT_DIR=$(cd $(dirname $0);pwd)
 echo "check_date" `date`"<BR>"
 
 while read DOMAIN
@@ -9,5 +9,4 @@ do
   date +"%s" --date="`openssl s_client -connect "$DOMAIN":443 -servername "$DOMAIN" </dev/null 2>/dev/null | openssl x509 -enddate -noout | cut -d'=' -f2`" | gawk '{printf("%d\n",($0-systime())/86400-1/86400+1)}'| tr '\n' ', '
   echo -e "\t$DOMAIN<BR>"
   echo -e "\r\n"
-done <check_url_list
-
+done <$SCRIPT_DIR/check_url_list
